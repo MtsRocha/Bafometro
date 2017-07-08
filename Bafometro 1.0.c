e0c8050a47c9335c214e93a803ccdfb5370f4b39
MtsRocha <mts_rocha2009@hotmail.com>
2017-07-08 11:42:46 -0300

    Update Bafometro 1.0.c

--- Bafometro 1.0.c
+++ Bafometro 1.0.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-FILE* AbreArquivo(char modo, char caminho[30]){ //Função que abre o arquivo, modo de abrir//caminho do arquivo
+FILE* AbreArquivo(char modo, char caminho[30]){ //Função que abre o arquivo(modo de abrir//caminho do arquivo)
     FILE *arquivo;
     switch(modo){
         case 'g':
